affed53fef50883b7f50d0a2fe2f999c915640a7
thilak0805 <daya.22.thilak@gmail.com>
2025-07-16 16:55:39 +0530

    java 9 - JPMS Java platform module system

---
+++ module2/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.modules;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
